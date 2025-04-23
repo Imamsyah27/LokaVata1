37157ee74af74ee8e3cc1bc64543fbef4b1802ba
Imamsyah27 <imamsyah.d33@gmail.com>
2025-04-23 21:50:30 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";  // Host database, biasanya 'localhost' jika menggunakan XAMPP
-$username = "root";         // Username MySQL, defaultnya 'root' pada XAMPP
-$password = "";             // Password MySQL, defaultnya kosong pada XAMPP
-$dbname = "lokg9647_livechat";       // Nama database yang sudah Anda buat
+$username = "ubuntu";         // Username MySQL, defaultnya 'root' pada XAMPP
+$password = "ubuntu";             // Password MySQL, defaultnya kosong pada XAMPP
+$dbname = "LokaVata";       // Nama database yang sudah Anda buat
 
 // Membuat koneksi
 $conn = new mysqli($servername, $username, $password, $dbname);
